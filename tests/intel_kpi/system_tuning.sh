d025f4d44f0fc58867ea3c6dff153541cbf72585
Song Yoong Siang <yoong.siang.song@intel.com>
2026-03-05 12:52:57 +0800

    Add system-level tuning script
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>

---
+++ tests/intel_kpi/system_tuning.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Copyright (C) 2026 Intel Corporation
+# Author Song Yoong Siang <yoong.siang.song@intel.com>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# System performance tuning for real-time KPI testing.
+#
+
+set -e
+
+cpupower -c 1 idle-set -d 0
+cpupower -c 1 idle-set -d 1
+cpupower -c 1 idle-set -d 2
+cpupower -c 1 idle-set -d 3
+cpupower -c 1 frequency-set --min 3700M --max 3700M -g performance
+tuna --cpus=1 --isolate
+
+echo "setting 620 MSR:"
+rdmsr -p 1 0x620
+wrmsr -p 1 0x620 0x2424
+rdmsr -p 1 0x620
+
+exit 0
