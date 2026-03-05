a47492126ddeac16543fa0cf0c13fbb69652df8a
Song Yoong Siang <yoong.siang.song@intel.com>
2026-03-05 12:52:57 +0800

    Tune: Add System Tuning, CAT, and stress-ng script
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>

---
+++ tests/intel_kpi/stress-ng.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+
+set -e
+
+echo Stress CPU2-7
+stress-ng --taskset 2-7 --cpu 6 --cpu-load 100 -t 0 >/dev/null &
+
+sleep 1
+ps -eLFc | head -n 1
+ps -eLFc | grep stress-ng
+date
+
+exit 0
