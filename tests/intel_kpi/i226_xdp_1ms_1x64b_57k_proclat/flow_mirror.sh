4559bbf2fa9a03cea28788a9f5a15cda88a4ce2f
Song Yoong Siang <yoong.siang.song@intel.com>
2026-03-05 12:57:25 +0800

    tests/intel_kpi: Add i226 XDP 1ms test with PTP hardware timestamp
    Add test case for Intel i226 NIC KPI evaluation with 1ms cycle time using
    AF_XDP zero-copy mode.
    
    Configuration highlights:
     - Enable processing latency metric
     - Workload on Reference: none
     - Workload on Mirror: 57k instructions per control loop (IPCL)
     - AF_XDP zero-copy mode with busy polling optimization
     - Disable interrupt coalescing for immediate interrupt firing
    
    Co-developed-by: Chwee-Lin Choong <chwee.lin.choong@intel.com>
    Signed-off-by: Chwee-Lin Choong <chwee.lin.choong@intel.com>
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>

---
+++ tests/intel_kpi/i226_xdp_1ms_1x64b_57k_proclat/flow_mirror.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# Copyright (C) 2026 Linutronix GmbH
+# Author Kurt Kanzenbach <kurt@linutronix.de>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Setup the Tx and Rx traffic flows for Intel i226 NIC(s) KPI testing.
+#
+
+set -e
+
+source ../../lib/common.sh
+source ../../lib/igc.sh
+
+#
+# Command line arguments.
+#
+INTERFACE=$1
+CYCLETIME_NS=$2
+BASETIME=$3
+
+[ -z $INTERFACE ] && INTERFACE="enp3s0"                          # default: enp3s0
+[ -z $CYCLETIME_NS ] && CYCLETIME_NS="1000000"                   # default: 1ms
+[ -z $BASETIME ] && BASETIME=$(date '+%s000000000' -d '-30 sec') # default: now - 30s
+
+load_kernel_modules
+
+napi_defer_hard_irqs "${INTERFACE}" "${CYCLETIME_NS}"
+
+igc_start "${INTERFACE}"
+ethtool -C ${INTERFACE} rx-usecs 0
+
+#
+# Split traffic between TSN High Stream and everything else.
+#
+ENTRY1_NS="500000" # Everything else
+ENTRY2_NS="500000" # TSN High Stream
+
+#
+# Tx Assignment with Qbv and full hardware offload.
+#
+# PCP 6 - Tx Q 1 - TSN High Stream
+# PCP X - Tx Q 0 - Everything else
+#
+tc qdisc replace dev ${INTERFACE} handle 100 parent root taprio num_tc 2 \
+  map 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 \
+  queues 1@0 1@1 \
+  base-time ${BASETIME} \
+  sched-entry S 0x03 ${ENTRY1_NS} \
+  sched-entry S 0x02 ${ENTRY2_NS} \
+  flags 0x02
+
+#
+# Rx Queues Assignment.
+#
+# PCP 6 - Rx Q 1 - TSN High Stream
+# PCP X - Rx Q 0 - Everything else
+RXQUEUES=(0 1 0 0 0 0 0 0 0 0)
+igc_rx_queues_assign "${INTERFACE}" RXQUEUES
+
+setup_irqs "${INTERFACE}"
+
+exit 0
