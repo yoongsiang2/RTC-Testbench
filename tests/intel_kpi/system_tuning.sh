eae549aa20f41a5a3d308e16ac1bc108106cf558
Song Yoong Siang <yoong.siang.song@intel.com>
2026-03-18 09:49:15 +0800

    tuning: set ptp4l and phc2sys to cpu0
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>

--- tests/intel_kpi/system_tuning.sh
+++ tests/intel_kpi/system_tuning.sh
@@ -14,12 +14,31 @@ cpupower -c 1 idle-set -d 0
 cpupower -c 1 idle-set -d 1
 cpupower -c 1 idle-set -d 2
 cpupower -c 1 idle-set -d 3
-cpupower -c 1 frequency-set --min 3700M --max 3700M -g performance
+cpupower -c 1 frequency-set --min 3100M --max 3100M -g performance
 tuna --cpus=1 --isolate
+cpupower -c 0 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 2 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 3 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 4 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 5 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 6 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 7 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 8 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 9 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 10 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 11 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 12 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 13 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 14 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 15 frequency-set --min 400M --max 3100M -g powersave
 
 echo "setting 620 MSR:"
 rdmsr -p 1 0x620
 wrmsr -p 1 0x620 0x2424
 rdmsr -p 1 0x620
+
+# Set CPU affinity to CPU 0
+sudo taskset -cp 0 $(pgrep ptp4l)
+sudo taskset -cp 0 $(pgrep phc2sys)
 
 exit 0
