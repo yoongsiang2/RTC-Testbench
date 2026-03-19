77967f1074e80f1a86bfafd334482e393a24ee31
Song Yoong Siang <yoong.siang.song@intel.com>
2026-03-19 08:43:01 +0800

    set other to 2.1G
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>

--- tests/intel_kpi/system_tuning.sh
+++ tests/intel_kpi/system_tuning.sh
@@ -16,21 +16,21 @@ cpupower -c 1 idle-set -d 2
 cpupower -c 1 idle-set -d 3
 cpupower -c 1 frequency-set --min 3100M --max 3100M -g performance
 tuna --cpus=1 --isolate
-cpupower -c 0 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 2 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 3 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 4 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 5 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 6 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 7 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 8 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 9 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 10 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 11 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 12 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 13 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 14 frequency-set --min 400M --max 3100M -g powersave
-cpupower -c 15 frequency-set --min 400M --max 3100M -g powersave
+cpupower -c 0 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 2 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 3 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 4 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 5 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 6 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 7 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 8 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 9 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 10 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 11 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 12 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 13 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 14 frequency-set --min 400M --max 2100M -g powersave
+cpupower -c 15 frequency-set --min 400M --max 2100M -g powersave
 
 echo "setting 620 MSR:"
 rdmsr -p 1 0x620
