5a7dc5b0502ea69c09556bae96f2370ba1faf23a
Song Yoong Siang <yoong.siang.song@intel.com>
2026-07-01 22:37:42 +0800

    2.9GHz and 2 RT cores
    Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>

--- tests/intel_kpi/stress-ng.sh
+++ tests/intel_kpi/stress-ng.sh
@@ -3,8 +3,8 @@
 
 set -e
 
-echo Stress CPU2-7
-stress-ng --taskset 2-7 --cpu 6 --cpu-load 100 -t 0 >/dev/null &
+echo Stress CPU3-8
+stress-ng --taskset 3-8 --cpu 6 --cpu-load 100 -t 0 >/dev/null &
 
 sleep 1
 ps -eLFc | head -n 1
